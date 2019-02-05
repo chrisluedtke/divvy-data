2bd986b56ef621a149bee701ad75282c8889c2d7
Chris Luedtke <chrisluedtke@gmail.com>
2019-02-05 11:07:23 -0600

    combine station and ride data into get_data() function

--- divvy/historical_data.py
+++ divvy/historical_data.py
@@ -5,72 +5,88 @@ from lxml import html
 import pandas as pd
 
 __all__ = [
-    'get_rides',
-    'get_stations'
+    'get_data',
 ]
 
-def get_rides(year, write_to:str = None):
+STN_DT_FORM = {
+    '2013': "%m/%d/%Y", # Not labeled for quarters
+    # '2014_Q1Q2': '', # xlsx file
+    '2014_Q3Q4': "%m/%d/%Y %H:%M",
+    # '2015': '', # no date column and not labeled for quarters
+    '2016_Q1Q2':"%m/%d/%Y",
+    '2016_Q3':"%m/%d/%Y",
+    '2016_Q4':"%m/%d/%Y",
+    '2017_Q1Q2':"%m/%d/%Y %H:%M:%S",
+    '2017_Q3Q4':"%m/%d/%Y %H:%M",
+}
+
+RD_DT_FORM = {
+    '2013':"%Y-%m-%d %H:%M", # Not labeled for quarters
+    '2014_Q1Q2':"%m/%d/%Y %H:%M",
+    '2014_Q3':"%m/%d/%Y %H:%M",
+    '2014_Q4':"%m/%d/%Y %H:%M",
+    '2015_Q1':"%m/%d/%Y %H:%M",
+    '2015_Q2':"%m/%d/%Y %H:%M",
+    '2015':"%m/%d/%Y %H:%M", # Q3 labeled as month integer
+    '2015_Q4':"%m/%d/%Y %H:%M",
+    '2016_Q1':"%m/%d/%Y %H:%M",
+    '2016':"%m/%d/%Y %H:%M", # Q2 labeled as month integer
+    '2016_Q3':"%m/%d/%Y %H:%M:%S",
+    '2016_Q4':"%m/%d/%Y %H:%M:%S",
+    '2017_Q1':"%m/%d/%Y %H:%M:%S",
+    '2017_Q2':"%m/%d/%Y %H:%M:%S",
+    '2017_Q3':"%m/%d/%Y %H:%M:%S",
+    '2017_Q4':"%m/%d/%Y %H:%M",
+    '2018_Q1':"%Y-%m-%d %H:%M:%S",
+    '2018_Q2':"%Y-%m-%d %H:%M:%S",
+    '2018_Q3':"%Y-%m-%d %H:%M:%S",
+    '2018_Q4':"%Y-%m-%d %H:%M:%S",
+}
+
+RD_COL_MAP = {
+    '01 - Rental Details Rental ID':'trip_id',
+    '01 - Rental Details Local Start Time':'start_time',
+    '01 - Rental Details Local End Time':'end_time',
+    '01 - Rental Details Bike ID':'bikeid',
+    '01 - Rental Details Duration In Seconds Uncapped':'tripduration',
+    '03 - Rental Start Station ID':'from_station_id',
+    '03 - Rental Start Station Name':'from_station_name',
+    '02 - Rental End Station ID':'to_station_id',
+    '02 - Rental End Station Name':'to_station_name',
+    'User Type':'usertype' ,
+    'Member Gender':'gender',
+    '05 - Member Details Member Birthday Year':'birthyear',
+    'stoptime':'end_time',
+    'starttime':'start_time',
+}
+
+
+def get_data(year, write_to:str = None, rides=False, stations=False):
+    """
+    Returns (pd.DataFrame of rides, pd.DataFrame of stations)
+    """
 #     cols = ['trip_id', 'start_time', 'end_time', 'bikeid', 'tripduration',
 #             'from_station_id', 'from_station_name', 'to_station_id',
 #             'to_station_name', 'usertype', 'gender', 'birthyear']
-
-    col_maps = {
-        '2018':{
-            '01 - Rental Details Rental ID':'trip_id',
-            '01 - Rental Details Local Start Time':'start_time',
-            '01 - Rental Details Local End Time':'end_time',
-            '01 - Rental Details Bike ID':'bikeid',
-            '01 - Rental Details Duration In Seconds Uncapped':'tripduration',
-            '03 - Rental Start Station ID':'from_station_id',
-            '03 - Rental Start Station Name':'from_station_name',
-            '02 - Rental End Station ID':'to_station_id',
-            '02 - Rental End Station Name':'to_station_name',
-            'User Type':'usertype' ,
-            'Member Gender':'gender',
-            '05 - Member Details Member Birthday Year':'birthyear'
-        },
-        '2016':{
-            'stoptime':'end_time',
-            'starttime':'start_time',
-        }
-    }
-
-    date_forms = {
-        '2016':{
-            'Q1':"%m/%d/%Y %H:%M",
-            'MO':"%m/%d/%Y %H:%M", # Month digit used in name
-            'Q3':"%m/%d/%Y %H:%M:%S",
-            'Q4':"%m/%d/%Y %H:%M:%S",
-        },
-        '2017':{
-            'Q1':"%m/%d/%Y %H:%M:%S",
-            'Q2':"%m/%d/%Y %H:%M:%S",
-            'Q3':"%m/%d/%Y %H:%M:%S",
-            'Q4':"%m/%d/%Y %H:%M",
-        },
-        '2018':{
-            'Q1':"%Y-%m-%d %H:%M:%S",
-            'Q2':"%Y-%m-%d %H:%M:%S",
-            'Q3':"%Y-%m-%d %H:%M:%S",
-            'Q4':"%Y-%m-%d %H:%M:%S",
-        }
-    }
-
     if isinstance(year, str):
         year = [year]
 
-    dfs = []
+    ride_dfs = []
+    station_dfs = []
 
     r = requests.get('https://www.divvybikes.com/system-data')
     webpage = html.fromstring(r.content)
 
     base_source = 'https://s3.amazonaws.com/divvy-data/tripdata/'
     for url in set(webpage.xpath('//a/@href')):
-        if not all(_ in url for _ in [base_source, '.zip']):
+        if base_source in url and url.endswith('.zip'):
+            pass
+        else:
             continue
 
-        z_year = re.findall(r'\d{4}', url.split('/')[-1])[0]
-        if not z_year in year:
+        z_fn = url.split('/')[-1]
+        z_year = re.findall(r'\d{4}', z_fn)[0]
+        if z_year not in year:
             continue
 
         print(url)
@@ -78,108 +94,70 @@ def get_rides(year, write_to:str = None):
         r = requests.get(url)
         with zipfile.ZipFile(io.BytesIO(r.content)) as z:
             if write_to:
-                file_name = (url.split('/')[-1]).split('.')[0]
-                z.extractall(f"data/{file_name}")
+                z.extractall(f"data/{z_fn.replace('.zip', '')}")
 
-            for fn in z.namelist():
-                if not (fn[-4:]=='.csv' and
-                        '_Trips_' in fn.split('/')[-1] and
-                        'MACOSX' not in fn):
+            for fpath in z.namelist():
+                fn = fpath.split('/')[-1]
+                if ((rides or stations) and fn.endswith(('.csv', '.xlsx'))
+                    and not fn.startswith('.')):
+
+                    quarter = re.findall('Q[1-4]', fn)
+                    if quarter:
+                        year_lookup = f"{z_year}_{''.join(quarter)}"
+                    else:
+                        year_lookup = z_year
+
+                else:
                     continue
 
-                quarter = re.findall(r'(Q\d)', fn.split('/')[-1])
-                if quarter:
-                    quarter = quarter[0]
-                else:
-                    quarter = 'MO'
+                if rides and '_Trips_' in fn:
+                    print(fn, z_year, ''.join(quarter))
+                    df = (pd.read_csv(z.open(fpath))
+                            .rename(columns=RD_COL_MAP))
 
-                print(fn, z_year, quarter)
-                df = pd.read_csv(z.open(fn))
+                    df['start_time'] = pd.to_datetime(
+                        df['start_time'], format=RD_DT_FORM[year_lookup],
+                        errors='coerce'
+                    )
 
-                if col_maps.get(z_year):
-                    df = df.rename(columns=col_maps.get(z_year))
+                    ride_dfs.append(df)
 
-                df['start_time'] = pd.to_datetime(
-                    df['start_time'], format=date_forms[z_year][quarter],
-                    errors='coerce'
-                )
+                elif stations and '_Stations_' in fn:
+                    print(fn, z_year, ''.join(quarter))
+                    if fn.endswith('.csv'):
+                        df = pd.read_csv(z.open(fpath))
+                        # handle dates here
+                    elif fn.endswith('.xlsx'):
+                        df = pd.read_excel(z.open(fpath))
 
-                dfs.append(df)
+                    df = df.rename(columns={
+                        'dateCreated':'online_date',
+                        'online date':'online_date',
+                    })
 
-    return pd.concat(dfs)
+                    if STN_DT_FORM.get(z_year):
+                        df['online_date'] = pd.to_datetime(
+                            df['online_date'], format=STN_DT_FORM[z_year],
+                            errors='coerce'
+                        )
+                    elif z_year=='2015':
+                        df['online_date'] = pd.to_datetime('2015', format="%Y")
+                    else:
+                        df['online_date'] = pd.to_datetime(
+                            df['online_date'], errors='coerce'
+                        )
 
+                    station_dfs.append(df)
 
-def get_stations(year, write_to:str = None):
-#     cols = ['trip_id', 'start_time', 'end_time', 'bikeid', 'tripduration',
-#             'from_station_id', 'from_station_name', 'to_station_id',
-#             'to_station_name', 'usertype', 'gender', 'birthyear']
-    date_forms = {
-        '2016':{
-            'Q1':"%m/%d/%Y %H:%M",
-            'MO':"%m/%d/%Y %H:%M", # Month digit used in name
-            'Q3':"%m/%d/%Y %H:%M:%S",
-            'Q4':"%m/%d/%Y %H:%M:%S",
-        },
-        '2017':{
-            'Q1':"%m/%d/%Y %H:%M:%S",
-            'Q2':"%m/%d/%Y %H:%M:%S",
-            'Q3':"%m/%d/%Y %H:%M:%S",
-            'Q4':"%m/%d/%Y %H:%M",
-        },
-        '2018':{
-            'Q1':"%Y-%m-%d %H:%M:%S",
-            'Q2':"%Y-%m-%d %H:%M:%S",
-            'Q3':"%Y-%m-%d %H:%M:%S",
-            'Q4':"%Y-%m-%d %H:%M:%S",
-        }
-    }
+    if ride_dfs:
+        ride_dfs = (pd.concat(ride_dfs, ignore_index=True)
+                      .sort_values('start_time'))
+        ride_dfs['tripduration'] = (ride_dfs.tripduration.astype(str)
+                                                         .str
+                                                         .replace(',', '')
+                                                         .astype(float))
+    if station_dfs:
+        station_dfs = (pd.concat(station_dfs, ignore_index=True)
+                         .sort_values('online_date'))
 
-    if isinstance(year, str):
-        year = [year]
-
-    dfs = []
-
-    r = requests.get('https://www.divvybikes.com/system-data')
-    webpage = html.fromstring(r.content)
-
-    base_source = 'https://s3.amazonaws.com/divvy-data/tripdata/'
-    for url in set(webpage.xpath('//a/@href')):
-        if not all(_ in url for _ in [base_source, '.zip']):
-            continue
-
-        z_year = re.findall(r'\d{4}', url.split('/')[-1])[0]
-        if not z_year in year:
-            continue
-
-        print(url)
-
-        r = requests.get(url)
-        with zipfile.ZipFile(io.BytesIO(r.content)) as z:
-            if write_to:
-                file_name = (url.split('/')[-1]).split('.')[0]
-                z.extractall(f"data/{file_name}")
-
-            for fn in z.namelist():
-                if not (fn[-4:]=='.csv' and
-                        '_Stations_' in fn.split('/')[-1] and
-                        'MACOSX' not in fn):
-                    continue
-
-                quarter = re.findall(r'(Q\d)', fn.split('/')[-1])
-                if quarter:
-                    quarter = quarter[0]
-                else:
-                    quarter = 'MO'
-
-                print(fn, z_year, quarter)
-                df = pd.read_csv(z.open(fn))
-                df = df.rename(columns={
-                    'dateCreated':'online_date',
-                    'online date':'online_date',
-                })
-
-                dfs.append(df)
-
-    df = (pd.concat(dfs)
-            .drop(columns=['city', 'Unnamed: 7', 'landmark']))
-    return df
+    return (ride_dfs, station_dfs)
